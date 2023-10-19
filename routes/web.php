c308ade7306a859207881d97d42bcd8524807b3b
motiq10 <motiq321@gmail.com>
2023-10-19 10:23:41 +0700

    push login2

--- routes/web.php
+++ routes/web.php
@@ -63,39 +63,4 @@ Route::get('/akun', function () {
     return view('kelola-akun.index');
 });
 
-Route::get('/akun', function () {
-    return view('kelola-akun.index');
-});
-
-
-Route::get('login',[AuthController::class,'index'])->name('login');
-Route::post('login',[AuthController::class,'check']);
-Route::middleware(['auth'])->group(function () {
-
-    Route::prefix('dashboard-superadmin')->middleware(['akses:superadmin'])->group(function () {
-        Route::get('/', [AkunController::class, 'index']);
-        Route::get('/tambah', [AkunController::class, 'create']);
-        Route::post('/simpan', [AkunController::class, 'store']);
-        Route::get('/edit/{id}', [AkunController::class, 'edit']);
-        Route::post('/edit/simpan', [AkunController::class, 'update']);
-        Route::delete('/hapus', [AkunController::class, 'destroy']);
-    });
-
- 
-    Route::prefix('dashboard-bendahara')->middleware(['akses:bendahara'])->group(function () {
-        Route::get('/', [RealisasiController::class, 'index']);
-        Route::get('/tambah', [RealisasiController::class, 'create']);
-        Route::post('/simpan', [RealisasiController::class, 'store']);
-        Route::get('/edit/{id}', [RealisasiController::class, 'edit']);
-        Route::post('/edit/simpan', [RealisasiController::class, 'update']);
-        Route::delete('/hapus', [RealisasiController::class, 'destroy']);
-    });
-
-    Route::prefix('dashboard-pemohon')->middleware(['akses:pemohon'])->group(function () {
-        Route::get('/', [PengajuanKebutuhanController::class, 'index']);
-        Route::post('/hapus', [PengajuanKebutuhanController::class, 'destroy']);
-    });
-
-    Route::get('/logout', [AuthController::class, 'logout']);
-});
 
